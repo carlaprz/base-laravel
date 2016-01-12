1206a337deb2093b111bfc8de114d49bf8427803
carla <carlaprz@gmail.com>
2016-01-12 15:49:00 +0100

    Add shipping_pvp to Orders database

--- database/migrations/2015_11_13_114726_cart_shipping_payments_coupons_orders.php
+++ database/migrations/2015_11_13_114726_cart_shipping_payments_coupons_orders.php
@@ -139,7 +139,8 @@ class CartShippingPaymentsCouponsOrders extends Migration
 
                 $table->float('total_pvp');
                 $table->float('total_iva');
-
+                $table->float('shipping_pvp');
+                
                 $table->integer('status')->unsigned();
                 $table->string('observations', 255);
                 $table->boolean('bill')->default(0);
@@ -232,8 +233,8 @@ class CartShippingPaymentsCouponsOrders extends Migration
             ));
         }
     }
-    
-     private function insertStatus()
+
+    private function insertStatus()
     {
         $status = json_decode(file_get_contents(__DIR__ . '/status.php'));
 
